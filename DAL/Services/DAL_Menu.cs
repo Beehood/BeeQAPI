e2b06132da27d9d5e14ed6e006f2b38baa653982
Swapnalisa Baliarsingh <swapnalisa.baliarsingh@beehood.com>
2026-05-05 12:41:12 +0530

    changes of dal menu

--- DAL/Services/DAL_Menu.cs
+++ DAL/Services/DAL_Menu.cs
@@ -1,26 +1,29 @@
 ﻿using DAL.ContractIF;
+using DAL.Dbcontext;
 using Dapper;
 using Models;
+using MySql.Data.MySqlClient;
 using System.Data;
+using static Org.BouncyCastle.Math.EC.ECCurve;
 
 public class DAL_Menu : IDAL_Menu
 {
-    private readonly IDbConnection _db;
 
-    public DAL_Menu(IDbConnection db)
+    private readonly DBConnection _config;
+
+    public DAL_Menu(DBConnection config)
     {
-        _db = db;
+        _config = config;
     }
+
 
     public async Task<List<MenuModel>> GetSidebar(string email)
     {
         try
         {
-            var result = await _db.QueryAsync<MenuModel>(
-                "sp_get_sidebar",
-                new { p_email = email },
-                commandType: CommandType.StoredProcedure
-            );
+            using var conn = new MySqlConnection(_config.DefaultConnection);
+
+            var result = await conn.QueryAsync<MenuModel>("sp_get_sidebar", new { p_email = email }, commandType: CommandType.StoredProcedure);
 
             return result?.ToList() ?? new List<MenuModel>();
         }
