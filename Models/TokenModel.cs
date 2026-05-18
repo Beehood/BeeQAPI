00189723588c1e9fb7f4aa6eff5647e6f1cd9324
Swapnalisa Baliarsingh <swapnalisa.baliarsingh@beehood.com>
2026-05-18 13:32:07 +0530

    updated model

--- Models/TokenModel.cs
+++ Models/TokenModel.cs
@@ -5,59 +5,72 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Models
-{
-    public class TokenModel
-    {
-        public long TokenId { get; set; }
+{   public class TokenModel
 
-        public long OrganizationId { get; set; }
+        {
 
-        public long BranchId { get; set; }
+            public long TokenId { get; set; }
 
-        public string? BranchName { get; set; }
+            public long OrganizationId { get; set; }
 
-        public long BranchServiceId { get; set; }
+            public long BranchId { get; set; }
 
-        public string? ServiceName { get; set; }
+            public long BranchServiceId { get; set; }
 
-        public long CustomerId { get; set; }
+            public int TokenNumber { get; set; }
 
-        public string? CustomerName { get; set; }
+            public DateTime? TokenDate { get; set; }
 
-        public string? CustomerPhone { get; set; }
+            public string? TokenDisplay { get; set; }
 
-        public int TokenNumber { get; set; }
+            public string? CustomerName { get; set; }
 
-        public string? TokenDisplay { get; set; }
+            public string? CustomerPhone { get; set; }
 
-        public DateTime TokenDate { get; set; }
+            public int Status { get; set; }
 
-        public int Status { get; set; }
+            public int Priority { get; set; }
 
-        public DateTime CreatedAt { get; set; }
+            public DateTime? CreatedAt { get; set; }
+
+            // EXTRA DISPLAY FIELDS
+
+            public string? OrganizationName { get; set; }
+
+            public string? BranchName { get; set; }
+
+            public string? ServiceName { get; set; }
+
+        }
+
+        public class TokenRequestDto
+
+        {
+
+            public long TokenId { get; set; }
+
+            public long OrganizationId { get; set; }
+
+            public long BranchId { get; set; }
+
+            public long BranchServiceId { get; set; }
+
+            public int TokenNumber { get; set; }
+
+            public DateTime? TokenDate { get; set; }
+
+            public string? TokenDisplay { get; set; }
+
+            public string? CustomerName { get; set; }
+
+            public string? CustomerPhone { get; set; }
+
+            public int Status { get; set; }
+
+            public int Priority { get; set; }
+
+        }
+
     }
 
-    public class TokenRequestDto
-    {
-        public long TokenId { get; set; }
-
-        public long OrganizationId { get; set; }
-
-        public long BranchId { get; set; }
-
-        public long BranchServiceId { get; set; }
-
-        public long CustomerId { get; set; }
-
-        public string? CustomerName { get; set; }
-
-        public string? CustomerPhone { get; set; }
-
-        public int Status { get; set; }
-
-        public string? Action { get; set; } // LIST / GETBYID / STATUS / CALLNEXT
-
-        public long CounterId { get; set; } // for counter operations
-    }
-}
 
